37dc96ed688e2f2701e877e9fc20560d7fcc87ff
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-10 14:25:49 +0100

    initialize device socket

--- real-time/namespaces/device/main.js
+++ real-time/namespaces/device/main.js
@@ -1,33 +1,61 @@
+const { deviceEntity } = require('../../../entities');
 const handleError = require('../handle_error');
 const { checkDevice } = require('./check_device');
 
 module.exports = (io) => {
-  const device = io.of('/device');
+  const deviceIO = io.of('/device');
 
-  device.use(async (socket, next) => {
+  deviceIO.use(async (socket, next) => {
     const deviceId = socket.handshake.query.deviceId;
-    const is_deviceValid = await checkDevice(deviceId);
-
-    if (is_deviceValid === null) {
-      return handleError(socket, {
+    const isDeviceValid = await checkDevice(deviceId);
+    
+    if (isDeviceValid === null) {
+      handleError(socket, {
         message: "deviceId: Missing or expired",
         status : 400
       });
+      
+    } else {
+      try {
+        const deviceId = isDeviceValid.deviceId;
+
+        const deviceProperties = {
+          current_visitors: 0,
+          max_visitors    : isDeviceValid.max_visitors,
+          socket_id       : socket.id,
+        };
+
+        deviceEntity.setNewDevice(deviceId, deviceProperties);
+        
+      } catch (error) {
+        console.log({error});
+      }
+
+      next();
+
     }
 
-    next();
-
   });
 
-  device.on('connection', socket => {
-    console.log('New connection is made');
-    socket.on('message', message => {
-      try {
-        console.log('a new message apperead ', message);
-      } catch (error) {
-        console.error('Error processing message:', error); 
-      }
+  deviceIO.on('connection', socket => {
+    
+    try {
+      const deviceId = deviceEntity.getDeviceIDBySocketId(socket.id);
+      const device   = deviceEntity.getDeviceById(deviceId);
+
+      deviceIO.to(socket.id)
+        .emit('handshake', JSON.stringify({
+          'current_visitors': device.current_visitors,
+          'max_visitors'    : device.max_visitors,
+        }));
+      
+    } catch (error) {
+      console.log({ error });
+    }
+
+    socket.on('disconnect', () => {
+      deviceEntity.removeSocketId(socket.id);
     });
   });
-
+  
 }
