150969b212b509cbf022c43aef10ed2e5ca34770
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-10 14:26:20 +0100

    error handler to emit error messages

--- real-time/namespaces/handle_error.js
+++ real-time/namespaces/handle_error.js
@@ -1,6 +1,13 @@
-module.exports = (io, { message, status = 400 }) => {
-  io.emit('error', {
-    message: message,
-    status: status
-  });
+module.exports = (socket, { message, status = 400 }) => {
+  try {
+    socket.send(JSON.stringify({
+        error: true,
+        message: message,
+        status: status
+      })
+    );
+
+  } catch (error) {
+    console.log('error sending the error message', error);
+  }
 }
